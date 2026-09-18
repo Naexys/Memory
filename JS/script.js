ce5c516cd69169449295357e7d481d33495775d6
Naexys <71014703+Naexys@users.noreply.github.com>
2026-09-18 12:32:26 +0200

    feat: dynamic DOM generation for game board with data-attributes and a11y properties

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,32 @@
-const dimension = 150
-const imgStart = Math.floor((Math.random() + 1 )* 100)
+const dimension = 150;
+const imgStart = Math.floor(Math.random()* 100 + 1);
 
-images = []
+const board = document.getElementById("game-board");
+
+images = [];
 for (let i  = 0; i < 8; i++) {
-    images.push('https://picsum.photos/${dimension}?random=${imgStart+i}')
+    images.push(`https://picsum.photos/seed/${dimension}?random=${imgStart+i}`);
 }
 
-let cards = [...images, ...images]
+let cards = [...images, ...images];
 
 function shuffle(array) {
     for (let i = 0; i < array.length - 2; i++){
-        let j = Math.floor(Math.random() * array.length)
-        [array[i], array[j] = array[j], array[i]]
+        let j = Math.floor(Math.random() * array.length);
+        [array[i], array[j]] = [array[j], array[i]];
     }
 }
+
+function initGame(){
+    shuffle(cards);
+    cards.forEach(imgUrl => {
+        let card = document.createElement("div");
+        card.className = "card";
+        card.dataset.value = imgUrl;
+        card.setAttribute("role", "button");
+        card.setAttribute("tabindex", "0");
+        board.appendChild(card);
+    })
+}
+
+initGame();
